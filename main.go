80f6a285538f88bc31096ec9beb4da10366fba07
RazikaBengana <razikabengana@gmail.com>
2024-06-04 15:06:36 +0200

    Add initial implementation of `home` and `about` page handlers

--- main.go
+++ main.go
@@ -5,15 +5,32 @@ import (
 	"net/http"
 )
 
-func main() {
+const portNumber = ":8080"
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello, world!")
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
-	})
-
-	_ = http.ListenAndServe(":8080", nil)
+// Home handles the home page requests
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the home page")
 }
+
+// About handles the about page requests
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValues(2, 2)
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
+}
+
+// addValues takes 2 integers and returns their sum
+func addValues(x, y int) int {
+	return x + y
+}
+
+// main is the entry point for the application
+func main() {
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	fmt.Println(fmt.Sprintf("Starrting application on port %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
+}
+
+// Functions that begin with a capital letter are accessible outside their package
+// (not applicable for the main package)
