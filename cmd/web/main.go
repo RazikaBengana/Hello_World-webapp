1f4525347b4156849d28712a68e88f97afd661b6
RazikaBengana <razikabengana@gmail.com>
2024-06-04 23:45:02 +0200

    Set up application configuration and template cache in `config.go`

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"github.com/RazikaBengana/Hello_World-webapp/pkg/config"
 	"github.com/RazikaBengana/Hello_World-webapp/pkg/handlers"
+	"github.com/RazikaBengana/Hello_World-webapp/pkg/render"
+	"log"
 	"net/http"
 )
 
@@ -10,6 +13,15 @@ const portNumber = ":8080"
 
 // main is the entry point for the application
 func main() {
+	var app config.AppConfig
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
 
