0d9df999c4ec3b82627179f04d6345a3d05fec63
RazikaBengana <razikabengana@gmail.com>
2024-06-05 12:29:16 +0200

    Switch routing library from `pat` to `chi` to add middleware support and enhance performance

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,16 +3,19 @@ package main
 import (
 	"github.com/RazikaBengana/Hello_World-webapp/pkg/config"
 	"github.com/RazikaBengana/Hello_World-webapp/pkg/handlers"
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"net/http"
 )
 
-// routes sets up the application routes using the pat router and handlers
+// routes sets up the application routes using the chi router and handlers
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	mux := chi.NewRouter()
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux.Use(middleware.Recoverer)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 
 	return mux
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/RazikaBengana/Hello_World-webapp
 
 go 1.12
 
-require github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f // indirect
+require github.com/go-chi/chi v4.1.2+incompatible
